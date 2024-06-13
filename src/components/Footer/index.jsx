4b4e3823e339a8c291f40398d38b52d408364a7c
quyen-ntx <xuanquyen1307@gmail.com>
2024-06-13 21:31:27 +0700

    bookstore website fe code

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Book Store Project ReactJS &copy; by Xuan Quyen</div>
+    </footer>
+  );
+};
+
+export default Footer;
